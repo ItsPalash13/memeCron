d63a3453fcceffb3e3f49e82a32152d35c034b2f
Palash Vishwas <84432951+ItsPalash13@users.noreply.github.com>
2023-07-28 22:54:25 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
-
+const axios = require("axios").default;
 
 exports.handler = async function(event, context) {
   const url = 'https://memeuserserver.onrender.com/hello/';
 const url2 = 'https://memegameserver.onrender.com/hello/' // Replace this with the actual URL of the server you want to send the request to
-
+console.log("Hello");
+  
 await axios.get(url)
   .then(response => {
     console.log('HTTP request sent successfully',response.data);
